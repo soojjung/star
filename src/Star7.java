6aeaa9586b4a4148630a3dc6bed6fea7ddf61db1
soojjung <89066676+soojjung@users.noreply.github.com>
2025-06-19 21:10:59 +0900

    feat: 나머지 문제들

--- src/Star7.java
+++ src/Star7.java
@@ -1,7 +1,7 @@
 public class Star7 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("*".repeat(5) + "*".repeat(i));
+            System.out.println("*".repeat(5 + i));
         }
     }
 }
